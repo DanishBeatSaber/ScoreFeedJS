0ecfae9e0772c38c2a0d39342414b331ed4f5896
Patrick Sørensen <hasfeldt@yahoo.dk>
2025-06-02 02:56:57 +0200

    Update Getmap-function

--- ScoreFeed_TS/src/helpers/getMap/index.ts
+++ ScoreFeed_TS/src/helpers/getMap/index.ts
@@ -2,10 +2,9 @@ export default async function getBeatSaverId(hash: string): Promise<string> {
     try {
         const response = await fetch("https://api.beatsaver.com/maps/hash/" + hash);
         const data = await response.json();
-        var id = data.id;
-        return id;
+        return data.id;
     } catch (error) {
         console.log(error);
-        return null ;
+        return null;
     }
 }
